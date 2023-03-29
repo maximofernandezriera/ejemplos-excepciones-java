8786fb25e7f37117b056cb786fb435b46112791a
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2023-03-29 20:39:26 +0200

    Create TratamientoExcepciones05.java

---
+++ TratamientoExcepciones05.java
@@ -0,0 +1,23 @@
+public class TratamientoExcepciones05 {
+
+	public static void main(String[] args) {
+		
+		try {
+			int a = 2;
+			int b = 0;
+			//1. Error de division entre 0	
+			int resultado = a/b;
+			String mensaje = null;
+			//2. Error de length de vacio
+			System.out.println(mensaje.length()); 					
+		} catch(ArithmeticException ex) {
+			//ex.printStackTrace();
+			System.err.println("Error de division: " + ex.getMessage());
+		} finally {
+			System.out.println("\nSe ha producido un error inesperado.");
+		}
+				
+
+	}
+
+}
